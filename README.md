4a01e63a8c4af5e9721c5859869557e6393d6c0e
armant <arman@seas1136.wireless-pennnet.upenn.edu>
2014-09-14 04:38:14 -0400

    README; fine-tuned firm-grabbing algorithm

---
+++ README.md
@@ -0,0 +1,16 @@
+Sources:
+<a href="http://en.wikipedia.org/wiki/List_of_companies_of_the_United_States" target="_blank">List of notable US firms</a>
+
+<a href="http://en.wikipedia.org/wiki/Category:Financial_services_companies_of_the_United_States" target="_blank">List of US financial services firms</a>
+
+<a href="http://en.wikipedia.org/wiki/List_of_investment_banks" target="_blank">List of investment banks</a>
+
+<a href="http://en.wikipedia.org/wiki/List_of_investment_banks" target="_blank">List of private equity firms</a>
+
+<a href="http://en.wikipedia.org/wiki/List_of_hedge_funds" target="_blank">List of hedge funds</a>
+
+<a href="http://seekingalpha.com/instablog/10942491-proprietary-trading/2482141-proprietary-trading-firms" target="_blank">List of proprietary trading firms</a>
+
+<a href="http://en.wikipedia.org/wiki/List_of_management_consulting_firms" target="_blank">List of management consulting firms</a>
+
+<a href="http://www.startupranking.com/top/united-states" target="_blank">List of top startups (top 300 US)</a>
--- grabFirms.js
+++ grabFirms.js
@@ -5,8 +5,35 @@
   var firmString = "var notable = [";
 
   for (i = 0; i < firms.length; i++) {
-    firmString += '["' + firms[i].innerHTML + '", "' + firms[i].href + '"], ';
+    var appeared = false;
+    for (j = 0; j < i; j ++) {
+      if (firms[j].innerHTML === firms[i].innerHTML) {
+        appeared = true;
+        break;
+      };
+    };
+      
+    if (!appeared) {
+      var finalString = firms[i].innerHTML;
+      
+      if (finalString.substring(0, 4) === 'The ') {
+        finalString = finalString.substring(4, finalString.length);
+      };
+      
+      if (finalString.substring(finalString.length - 6,
+          finalString.length) === ', Inc.') {
+        finalString = finalString.substring(0, finalString.length - 6);
+      };
+    
+      if (finalString.substring(finalString.length - 5,
+          finalString.length) === ' Inc.') {
+        finalString = finalString.substring(0, finalString.length - 5);
+      };
+        
+      firmString += '["' + finalString + '", "' +
+          firms[i].href + '"], ';
       console.log(firms[i].href);
+    };
   };
   
   firmString += '];';
